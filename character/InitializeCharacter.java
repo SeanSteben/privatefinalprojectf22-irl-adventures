a7732bfee551f00fa40749f11bafc43c8627fb6c
Dylan Fassio <dfassio15@gmail.com>
2022-10-24 16:46:16 -0400

    add character package

--- character/InitializeCharacter.java
+++
@@ -1,9 +0,0 @@
-package character;
-
-public class InitializeCharacter {
-
-	public static void main(String[] args) {
-		Character player = Character.getInstance();
-		System.out.println(player.getStr());
-	}
-}
