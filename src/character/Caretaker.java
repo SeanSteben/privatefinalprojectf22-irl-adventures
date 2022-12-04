7020c983642afdb3843080eda2adbd6294791eb6
Dylan Fassio <dfassio15@gmail.com>
2022-12-04 00:50:43 -0500

    load method reads String from text file for last Saved game

--- src/character/Caretaker.java
+++ src/character/Caretaker.java
@@ -2,7 +2,10 @@ package character;
 
 import character.Character.Memento;
 import java.io.FileWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Caretaker class that stores and returns the last saved Memento object.
@@ -25,14 +28,26 @@ public class Caretaker {
 			saver.close();
 			System.out.println("Character successfully saved!"); //integrate with UI
 		} catch (IOException e) {
-			System.out.println("An error occured");
+			System.out.println("Couldn't write to file");
 			e.printStackTrace();
 		}
 	}
 	
 	//returns the stored Memento object
 	public void load() {
-		player.load(lastSave);
+		
+		try {
+			File save = new File("SaveData.txt");
+			Scanner loader = new Scanner(save);
+			String saveData = loader.nextLine();
+			loader.close();
+			player.load(saveData);
+		} catch (FileNotFoundException e) {
+			System.out.println("File could not be found");
+			e.printStackTrace();
+		}
+		
+
 	}
 	
 	
