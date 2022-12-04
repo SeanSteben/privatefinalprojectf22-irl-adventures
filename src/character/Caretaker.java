3d0e7d428ed6b7ffa552a990f99cbc7e78e20139
Dylan Fassio <dfassio15@gmail.com>
2022-12-04 00:19:46 -0500

    save method writes character data to text file

--- src/character/Caretaker.java
+++ src/character/Caretaker.java
@@ -1,6 +1,8 @@
 package character;
 
 import character.Character.Memento;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Caretaker class that stores and returns the last saved Memento object.
@@ -16,10 +18,22 @@ public class Caretaker {
 	//sets the local Memento object to the most recent version of the character.
 	public void save() {
 		lastSave = player.save();
+		
+		try {
+			FileWriter saver = new FileWriter("SaveData.txt");
+			saver.write(lastSave.toString());
+			saver.close();
+			System.out.println("Character successfully saved!"); //integrate with UI
+		} catch (IOException e) {
+			System.out.println("An error occured");
+			e.printStackTrace();
+		}
 	}
 	
 	//returns the stored Memento object
 	public void load() {
 		player.load(lastSave);
 	}
+	
+	
 }
