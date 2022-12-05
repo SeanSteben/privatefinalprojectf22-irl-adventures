73be5e87a07c455d6eb8960c96ce29c1eae53307
SeanSteben <114422618+SeanSteben@users.noreply.github.com>
2022-12-04 22:58:36 -0500

    Update TiringDay.java

--- src/normalday/TiringDay.java
+++ src/normalday/TiringDay.java
@@ -1,7 +1,7 @@
-package NormalDay;
-import irlPackage.*;
+package normalday;
+import irlpackage.*;
 import java.util.Scanner;
-import RandomEvent.RandomEvent;
+import randomEvent.RandomEvent;
 import character.Character;
 /**
  * 
@@ -9,14 +9,16 @@ import character.Character;
  * TiringDay is the state of which the character performs tired and exhausted. They receive less XP than normal  for completing tasks in this state
  *
  */
-//Normal day State
 public class TiringDay implements DayState {
-	
-	 public void goAboutDay(Character player, PlayerDay state) {
-		int thisState = 3;
+	int thisState = 3;
+	/**
+	 * method is the framework the day to day activity tracking in a tired mood. implements the tasks as needed, starts the random events if they occur
+	 */
+	public void goAboutDay(Character player, PlayerDay currentDay) {
+		
 		int choice;
-		Scanner scan = new Scanner(System.in);
-			System.out.println("This is a tiring  day!");
+
+			System.out.println("This is a tiring day!");
 			System.out.println("Your current stats: " + "\n" + 
 					"Strength: " + player.getStr() + "\n" +
 					"Dexterity: " + player.getDex() + "\n" +
@@ -25,12 +27,29 @@ public class TiringDay implements DayState {
 					"Charisma: " + player.getChr() + "\n" +
 					"Constituion: " + player.getCon() + "\n" +
 					"Kindness: " + player.getKnd()+ "\n");
-			System.out.println("What activity would you like to do?"+ "\n" +
-					"1) Exercise" + "\n" +
-					"2) Study" + "\n" + 
-					"3) Socialize" + "\n" +
-					"4) Trigger RandomEvent **TESTER TO CHANGE STATE** ");
-			choice = scan.nextInt();
+			Random r = new Random();
+			if(r.nextInt(10)  == 0 && player.getTime() >= 30)//match random and do not trigger if less than 30 minutes
+			{
+				System.out.println("Random Event Triggered");//tester text
+				RandomEvent rand = new RandomEvent();
+				rand.startRand(player);
+				currentDay.notifyObservers();
+				player.removeTime(30);//random Event takes up 30 minutes
+			}
+			Scanner scan = new Scanner(System.in);
+			do
+			{
+				System.out.println("What activity would you like to do?"+ "\n" +
+						"1) Exercise" + "\n" +
+						"2) Study" + "\n" + 
+						"3) Socialize" + "\n");
+				while(!scan.hasNextInt()) 
+				{
+					System.out.println("Please enter a number");
+					scan.next();
+				}
+				choice = scan.nextInt();
+			}while(choice<1 || choice>4);
 			if(choice == 1) {
 				Exercise ex = new Exercise();
 				ex.doActivity(player,thisState);
@@ -44,22 +63,17 @@ public class TiringDay implements DayState {
 				soc.doActivity(player,thisState);
 			}
 			
-			//tester for change into RandomEvent State
+			//tester option to start random event
 			if(choice == 4)
 			{
-				System.out.println("Random Event Triggered");//tester text
+				System.out.println("Random Event Triggered");
 				RandomEvent rand = new RandomEvent();
 				rand.startRand(player);
-				state.eventOccured();
-				//return rand;
-				
+				currentDay.notifyObservers();
 				
 				
 			}
-		
-	}
-
-	
+	 }
 
 	
 }
